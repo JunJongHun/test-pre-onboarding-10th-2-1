31f13176cb46a52d530b6199779a3141352b7e2b
jonghun <minn4072@gmail.com>
2023-05-01 23:00:12 +0900

    feat: app 컴포넌트 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div className="App">Template</div>;
+
+  return <>
+  <div className="App">Template</div>;
+  <div>여러분 과제 화이팅 해봅시다💪💪💪</div>
+  </>
 };
 
 export default App;
